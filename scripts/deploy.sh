a521fc182511fddeb3c58975d55947c3dec861d5
Nick Vorobiov <nickvorobiov@yandex.ru>
2016-10-25 10:54:45 +0300

    no message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 if [[ $TRAVIS_BRANCH == 'gh-pages' ]] ; then
-  cd _site
-
-  printenv
-
-  # We redirect any output to
-  # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  #git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
-
   chmod 600 deploy-key
   mv deploy-key ~/.ssh/id_rsa
 
