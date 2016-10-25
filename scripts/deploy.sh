5ba95edbb9814e8a034bed539d1560774cc6fab9
Nick Vorobiov <nickvorobiov@yandex.ru>
2016-10-25 10:46:47 +0300

    no message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+set -e
 
 if [[ $TRAVIS_BRANCH == 'gh-pages' ]] ; then
   cd _site
-  git init
 
-  git config user.name "Travis CI"
-  git config user.email "feriese@microsoft.com"
-
-  git add .
-  git commit -m "Deploy"
+  printenv
 
   # We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
+  #git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
+
+  chmod 600 deploy-key
+  mv deploy-key ~/.ssh/id_rsa
+
+  rsync -a -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=quiet" --quiet --omit-dir-times --update --delete --delay-updates _site/ ${DEPLOY_ADR}
+
 else
   echo 'Invalid branch. You can only deploy from gh-pages.'
   exit 1
